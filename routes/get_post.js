9732189c5d34987fd24e1016e067cf93276f753e
DuranByrm01 <duranb895@gmail.com>
2024-11-23 11:48:58 +0300

    ok

--- routes/get_post.js
+++ routes/get_post.js
@@ -70,8 +70,38 @@ router.get('/urunler/malzeme/get/:urun_malzeme_PK', async function (req, res) {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+router.post('/urunler/stok/post', async (req, res) => {
+    const { product, material, amount } = req.body;
   
+    console.log("Gönderilen Veriler:", req.body);
+    
+     // `material` içinden ID'yi çek (örneğin: "TRC01 PCB (12)" -> ID: 12)
+     const materialIdMatch = material.match(/\((\d+)\)/); // Parantez içindeki sayıyı yakala
+     const materialId = materialIdMatch ? parseInt(materialIdMatch[1], 10) : null;
+    
+     // Veritabanında malzemeyi bul
+    const [rows] = await db.query("SELECT urun_malzeme_adet FROM urunmalzemeleri WHERE malzeme_id = ?", [materialId]);
+
+    const currentStock = rows[0].stok; // Mevcut stok miktarı
+    const newStock =
+      action === "add" ? currentStock + parseInt(amount, 10) : currentStock - parseInt(amount, 10);
+
+
+    // Stok miktarını güncelle
+    await db.query("UPDATE urunmalzemeleri SET urun_malzeme_adet = ? WHERE malzeme_id = ?", [newStock, materialId]);
+
+    res.status(200).json({
+        success: true,
+        message: "Stok başarıyla güncellendi.",
+        updatedStock: newStock,
+      });
+
+    
+});
+  
+  
+
+
 
 
   
