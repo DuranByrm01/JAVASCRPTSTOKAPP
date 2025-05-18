7015e46346417e69d84fbe9b5549935434e66e6b
DuranByrm01 <duranb895@gmail.com>
2025-05-18 17:08:06 +0300

    ok

--- config.js
+++ config.js
@@ -1,11 +1,25 @@
+// const config = {
+//     db: {
+//         host: "localhost",
+//         user: "root",
+//         password:"bayram22",
+//         database:"stockapp"
+//     }
+
+// }
+
+
+require("dotenv").config(); // .env dosyasını oku
+
 const config = {
     db: {
-        host: "localhost",
-        user: "root",
-        password:"bayram22",
-        database:"stockapp"
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     }
-
-}
+};
 
 module.exports = config;
+
+
--- index.js
+++ index.js
@@ -1,30 +1,56 @@
-const express = require("express");
+// const express = require("express");
 
+// const app = express();
+
+// app.set("view engine", "ejs");
+
+
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+
+// const userRoutes = require("./routes/users");
+// const adminRoutes = require("./routes/admin");
+// const get_post = require("./routes/get_post");
+
+
+// app.use(express.static('public'));
+
+// app.use(express.static("node_modules"));
+
+// app.use("/admin",adminRoutes);
+// app.use(userRoutes);
+// app.use(get_post);
+
+
+
+
+// app.listen(3000,function(){
+//     console.log("port 3000 çalışıyor");
+    
+// });
+
+
+const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
 
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const userRoutes = require("./routes/users");
 const adminRoutes = require("./routes/admin");
 const get_post = require("./routes/get_post");
 
-
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 app.use(express.static("node_modules"));
 
-app.use("/admin",adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(userRoutes);
 app.use(get_post);
 
-
-
-
-app.listen(3000,function(){
-    console.log("port 3000 çalışıyor");
-    
+// ✅ PORT ayarını Railway'e uygun hale getir
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+    console.log(`Uygulama çalışıyor: http://localhost:${PORT}`);
 });
