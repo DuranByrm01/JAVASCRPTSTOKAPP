e02e82bcf7c9a1b161e4fab613264d8338b35005
DuranByrm01 <duranb895@gmail.com>
2025-05-18 17:47:19 +0300

    ok

--- routes/test.js
+++ routes/test.js
@@ -11,3 +11,18 @@ const db = require("../data/db");
 
 
 module.exports = router;
+
+
+
+
+///////////////////////////////
+
+DB_HOST=localhost
+DB_USER=root
+DB_PASSWORD=bayram22
+DB_NAME=stockapp
+
+
+
+
+////////////////////////
