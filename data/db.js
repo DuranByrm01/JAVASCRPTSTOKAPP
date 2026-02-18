dc215a66477a9f7efb222e43427121a01e9330c8
DuranByrm01 <duranb895@gmail.com>
2026-02-18 21:13:33 +0300

    db connect

--- data/db.js
+++ data/db.js
@@ -1,34 +1,34 @@
-const mysql = require("mysql2");
-const config = require("../config");
-
-let connection = mysql.createConnection(config.db);
-
-connection.connect(function (err) {
-  if (err) {
-    return console.log(err);
-  }
-
-
-  console.log("mysql bağlantısı başarılı...")
-});
-
-module.exports = connection.promise();
-
-
-
-// const mysql = require("mysql2/promise"); // promise destekli mysql2
-
+// const mysql = require("mysql2");
 // const config = require("../config");
 
-// const pool = mysql.createPool({
-//   host: config.db.host,
-//   user: config.db.user,
-//   password: config.db.password,
-//   database: config.db.database,
-//   port: config.db.port || 3306,
-//   waitForConnections: true,
-//   connectionLimit: 10, // istediğin kadar bağlanma limiti koyabilirsin
-//   queueLimit: 0
+// let connection = mysql.createConnection(config.db);
+
+// connection.connect(function (err) {
+//   if (err) {
+//     return console.log(err);
+//   }
+
+
+//   console.log("mysql bağlantısı başarılı...")
 // });
 
-// module.exports = pool;
+// module.exports = connection.promise();
+
+
+
+const mysql = require("mysql2/promise"); // promise destekli mysql2
+
+const config = require("../config");
+
+const pool = mysql.createPool({
+  host: config.db.host,
+  user: config.db.user,
+  password: config.db.password,
+  database: config.db.database,
+  port: config.db.port || 3306,
+  waitForConnections: true,
+  connectionLimit: 10, // istediğin kadar bağlanma limiti koyabilirsin
+  queueLimit: 0
+});
+
+module.exports = pool;
